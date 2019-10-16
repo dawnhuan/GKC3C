770f6216877deb3bab83641dca4042281a1d9648
liuquan98 <liuquan2017@sjtu.edu.cn>
2019-10-16 17:14:47 +0800

    Added bluetooth tool class and a basic UI for activity_main. Contents, names and functions need to be updated for the Buttons in the middle. They can be used to open up new activities or send immediate message via bluetooth as shown in activity_main.

---
+++ app/src/main/java/com/example/a3cteamworkapplication/BluetoothTool.java
@@ -0,0 +1,153 @@
+package com.example.a3cteamworkapplication;
+
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.os.Handler;
+import android.os.Message;
+import android.os.SystemClock;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.lang.reflect.Method;
+import java.util.UUID;
+
+public class BluetoothTool {
+
+    private static final String TAG = "BluetoothTool";
+
+    private BluetoothDevice device;
+    private BluetoothSocket socket;
+
+    private ReadTask readTask;
+
+    private Handler handler;
+
+    public static final int CONNECT_FAILED = 1;
+    public static final int CONNECT_SUCCESS = 5;
+    public static final int READ_FAILED = 2;
+    public static final int WRITE_FAILED = 3;
+    public static final int DATA = 4;
+    public static final int DISCONNECT = 6;
+
+    private boolean isConnect = false;
+
+    private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
+    public BluetoothTool(BluetoothDevice device, Handler handler) {
+        this.device = device;
+        this.handler = handler;
+    }
+
+    public void connect() {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                BluetoothSocket tmp = null;
+                Method method;
+                try {
+                    method = device.getClass().getMethod("createRfcommSocketToServiceRecord", UUID.class);
+                    tmp = (BluetoothSocket) method.invoke(device, MY_UUID);
+                } catch (Exception e) {
+                    setState(CONNECT_FAILED);
+                    Log.e(TAG, e.toString());
+                }
+                socket = tmp;
+                try {
+                    socket.connect();
+                    isConnect = true;
+                    setState(CONNECT_SUCCESS);
+                    BluetoothTool.this.readTask = new ReadTask();
+                    BluetoothTool.this.readTask.start();
+                } catch (Exception e) {
+                    setState(CONNECT_FAILED);
+                    Log.e(TAG, e.toString());
+                }
+            }
+        });
+        new Thread(thread).start();
+    }
+
+    public void disconnect() {
+        try {
+            socket.close();
+            socket = null;
+            if(readTask != null) {
+                readTask.join();
+            }
+            isConnect = false;
+            setState(DISCONNECT);
+        } catch (Exception e) {
+            Log.e(TAG, e.toString());
+        }
+    }
+
+    public class ReadTask extends Thread {
+        @Override
+        public void run() {
+            byte[] buffer = new byte[1024];
+            int bytes;
+            InputStream inputStream;
+            while(true) {
+                try {
+                    inputStream = socket.getInputStream();
+                    bytes = inputStream.available();
+                    if(bytes != 0) {
+                        SystemClock.sleep(100); // pause and wait for the rest of the data
+                        bytes = inputStream.available();
+                        bytes = inputStream.read(buffer, 0, bytes);
+                        byte[] buf_data = new byte[bytes];
+                        for(int i = 0; i < bytes; i++)
+                            buf_data[i] = buffer[i];
+                        String s = new String(buf_data);
+                        Log.d(TAG, String.valueOf(bytes));
+                        Log.d(TAG, s);
+                        Message msg = handler.obtainMessage();
+                        msg.what = DATA;
+                        msg.obj = s;
+                        handler.sendMessage(msg);
+                    }
+                } catch (IOException e) {
+                    setState(READ_FAILED);
+                    Log.e(TAG, e.toString());
+                    break;
+                }
+            }
+            if(socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    Log.e(TAG, e.toString());
+                }
+            }
+        }
+    }
+
+    public class WriteTask extends Thread {
+        private String str;
+
+        public WriteTask(String str) {
+            this.str = str;
+        }
+
+        @Override
+        public void run() {
+            OutputStream outputStream = null;
+            byte[] out_data = str.getBytes();
+            try {
+                outputStream = socket.getOutputStream();
+                outputStream.write(out_data);
+            } catch (Exception e) {
+                setState(WRITE_FAILED);
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void setState(int mes) {
+        Message message = new Message();
+        message.what = mes;
+        handler.sendMessage(message);
+    }
+}
--- app/src/main/java/com/example/a3cteamworkapplication/MainActivity.java
+++ app/src/main/java/com/example/a3cteamworkapplication/MainActivity.java
@@ -1,17 +1,405 @@
 package com.example.a3cteamworkapplication;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    int a = 1;
-    int b = 2;
+    private static final String TAG = "MainActivity";
+
+    private static final int SCAN_PERIOD = 1000;
+
+    private TextView statusTV;
+    private Button searchBtn;
+    private Button stopSearchBtn;
+    private Button disconnectBtn;
+    private ListView devicesLV;
+    private Button startBtn, armBtn, takeoffBtn, killBtn, disarmBtn, manualBtn;
+    private ScrollView scrollView;
+    private TextView receiveTV;
+    private EditText sendET;
+    private Button sendBtn;
+
+    private ArrayAdapter<String> adapter;
+
+    private boolean searching = false;
+
+    private List<String> devicesArrayList = new ArrayList<>();
+
+    private BluetoothReceiver bluetoothReceiver;
+    private BluetoothAdapter bluetoothAdapter;
+
+    private BluetoothTool client;
+
+    private static final int REQUEST_ENABLE = 1;
+    private static final int REQUEST_DISCOVERABLE = 2;
+
+    private StringBuilder receiveStringBuilder = new StringBuilder();
+
+    private long exitTime;
+
+    @SuppressLint("HandlerLeak")
+    private final Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case BluetoothTool.CONNECT_FAILED:
+                    showStatus("连接失败");
+                    try {
+                        client.connect();
+                    } catch (Exception e) {
+                        Log.e(TAG, e.toString());
+                    }
+                    break;
+                case BluetoothTool.CONNECT_SUCCESS:
+                    showStatus("连接成功");
+                    searchBtn.setEnabled(false);
+                    disconnectBtn.setEnabled(true);
+                    startBtn.setEnabled(true);
+                    armBtn.setEnabled(true);
+                    takeoffBtn.setEnabled(true);
+                    killBtn.setEnabled(true);
+                    disarmBtn.setEnabled(true);
+                    manualBtn.setEnabled(true);
+                    devicesLV.setEnabled(false);
+                    sendBtn.setEnabled(true);
+                    break;
+                case BluetoothTool.READ_FAILED:
+                    showStatus("读取失败");
+                    break;
+                case BluetoothTool.WRITE_FAILED:
+                    showStatus("写入失败");
+                    break;
+                case BluetoothTool.DATA:
+                    receiveStringBuilder.append((String)msg.obj);
+                    showReceiveString(receiveStringBuilder.toString());
+                    break;
+                case BluetoothTool.DISCONNECT:
+                    showStatus("已断开连接");
+                    searchBtn.setEnabled(true);
+                    stopSearchBtn.setEnabled(false);
+                    disconnectBtn.setEnabled(false);
+                    startBtn.setEnabled(false);
+                    armBtn.setEnabled(false);
+                    takeoffBtn.setEnabled(false);
+                    killBtn.setEnabled(false);
+                    disarmBtn.setEnabled(false);
+                    manualBtn.setEnabled(false);
+                    devicesLV.setEnabled(true);
+                    sendBtn.setEnabled(false);
+                    break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupUI();
+
+        enableBluetooth();
+
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        bluetoothReceiver = new BluetoothReceiver();
+        registerReceiver(bluetoothReceiver, filter);
+        filter = new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        registerReceiver(bluetoothReceiver, filter);
+    }
+
+    private void setupUI() {
+        statusTV = (TextView) findViewById(R.id.statusTV);
+        searchBtn = (Button) findViewById(R.id.searchBtn);
+        stopSearchBtn = (Button) findViewById(R.id.stopSearchBtn);
+        disconnectBtn = (Button) findViewById(R.id.disconnectBtn);
+        devicesLV = (ListView) findViewById(R.id.devicesLV);
+
+        startBtn = (Button) findViewById(R.id.someBtn);
+        armBtn = (Button) findViewById(R.id.anotherBtn);
+        takeoffBtn = (Button) findViewById(R.id.oneMoreBtn);
+        killBtn = (Button) findViewById(R.id.againOneBtn);
+        disarmBtn = (Button) findViewById(R.id.dioBtn);
+        manualBtn = (Button) findViewById(R.id.finalBtn);
+        scrollView = (ScrollView) findViewById(R.id.scroll);
+        receiveTV = (TextView) findViewById(R.id.receiveTV);
+        sendET = (EditText) findViewById(R.id.sendET);
+        sendBtn = (Button) findViewById(R.id.sendBtn);
+
+        stopSearchBtn.setEnabled(false);
+        disconnectBtn.setEnabled(false);
+        startBtn.setEnabled(false);
+        armBtn.setEnabled(false);
+        takeoffBtn.setEnabled(false);
+        killBtn.setEnabled(false);
+        disarmBtn.setEnabled(false);
+        manualBtn.setEnabled(false);
+        sendBtn.setEnabled(false);
+
+        searchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                searching = true;
+                searchBtn.setEnabled(false);
+                stopSearchBtn.setEnabled(true);
+                showStatus("搜索中...");
+                devicesArrayList.clear();
+                showDevices();
+                if(bluetoothAdapter != null)
+                    bluetoothAdapter.startDiscovery();
+            }
+        });
+
+        stopSearchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                searching = false;
+                stopSearchBtn.setEnabled(false);
+                searchBtn.setEnabled(true);
+                if(bluetoothAdapter != null)
+                    bluetoothAdapter.cancelDiscovery();
+            }
+        });
+
+        disconnectBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    client.disconnect();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, devicesArrayList);
+        devicesLV.setAdapter(adapter);
+        devicesLV.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(searching) {
+                    bluetoothAdapter.cancelDiscovery();
+                    searching = false;
+                    searchBtn.setEnabled(true);
+                    stopSearchBtn.setEnabled(false);
+                }
+                String str = devicesArrayList.get(position);
+                String macAddress = str.split(" \\| ", 2)[1];
+                Toast.makeText(MainActivity.this, macAddress, Toast.LENGTH_SHORT).show();
+                BluetoothDevice device = bluetoothAdapter.getRemoteDevice(macAddress);
+                client = new BluetoothTool(device, handler);
+                try {
+                    client.connect();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Toast.makeText(MainActivity.this, e.toString(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showConfirmDialog("start\n");
+            }
+        });
+
+        armBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showConfirmDialog("test\n");
+            }
+        });
+
+        takeoffBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showConfirmDialog("arm\n");
+                showConfirmDialog("takeoff\n");
+            }
+        });
+
+        killBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //showConfirmDialog("kill\n");
+                BluetoothTool.WriteTask writeTask = client.new WriteTask("disarm\n");
+                writeTask.start();
+                Toast.makeText(MainActivity.this, "已发送", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        disarmBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showConfirmDialog("kill\n");
+            }
+        });
+
+        manualBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showConfirmDialog("manual\n");
+            }
+        });
+
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BluetoothTool.WriteTask writeTask = client.new WriteTask(sendET.getText().toString() + "\n");
+                writeTask.start();
+            }
+        });
+    }
+
+    private void enableBluetooth() {
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if(!bluetoothAdapter.isEnabled()) {
+            bluetoothAdapter.enable();
+        }
+        Intent enable = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+        enable.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+        startActivityForResult(enable, REQUEST_DISCOVERABLE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case REQUEST_DISCOVERABLE:
+                if(resultCode == Activity.RESULT_CANCELED) {
+                    showStatus("蓝牙已关闭");
+                } else {
+                    showStatus("蓝牙已打开");
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        unregisterReceiver(bluetoothReceiver);
+        super.onDestroy();
+    }
+
+    private class BluetoothReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if(BluetoothDevice.ACTION_FOUND.equals(action)) {
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                String str = device.getName() + " | " + device.getAddress();
+                Log.d(TAG, str);
+                if(devicesArrayList.indexOf(str) == -1)
+                    devicesArrayList.add(str);
+                showDevices();
+            }
+            else if(BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
+                showStatus("已停止搜索");
+                searching = false;
+                searchBtn.setEnabled(true);
+                stopSearchBtn.setEnabled(false);
+            }
+        }
+    }
+
+    private void showStatus(final String string) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                statusTV.setText(string);
+            }
+        });
+    }
+
+    private void showDevices() {
+        adapter.notifyDataSetChanged();
+        devicesLV.smoothScrollToPosition(devicesArrayList.size() - 1);
+    }
+
+    private void showReceiveString(final String string) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                receiveTV.setText(string);
+                scrollView.fullScroll(ScrollView.FOCUS_DOWN);
+            }
+        });
+    }
+
+    private void showConfirmDialog(final String action) {
+        String str = "";
+        if(action.equals("start\n")) {
+            str = "启动";
+        } else if(action.equals("arm\n")) {
+            str = "解锁";
+        } else if(action.equals("takeoff\n")) {
+            str = "起飞";
+        } else if(action.equals("kill\n")) {
+            str = "紧急停止";
+        } else if(action.equals("disarm\n")) {
+            str = "锁定";
+        } else if(action.equals("manual\n")) {
+            str = "手动控制";
+        } else if(action.equals("test\n")) {
+            str = "启用视觉";
+        }
+
+        AlertDialog.Builder confirmDialog = new AlertDialog.Builder(MainActivity.this);
+        confirmDialog.setTitle("确认");
+        confirmDialog.setMessage("确认 " + str + " ?");
+        confirmDialog.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                BluetoothTool.WriteTask writeTask = client.new WriteTask(action);
+                writeTask.start();
+                Toast.makeText(MainActivity.this, "已发送", Toast.LENGTH_SHORT).show();
+            }
+        });
+        confirmDialog.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        confirmDialog.show();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if(System.currentTimeMillis() - exitTime > 2000) {
+            Toast.makeText(this, "再按一次退出程序", Toast.LENGTH_SHORT).show();
+            exitTime = System.currentTimeMillis();
+        } else {
+            finish();
+        }
+        return true;
     }
 }
